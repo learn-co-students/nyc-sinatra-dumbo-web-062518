17a26537d93ecac84aedaae50bc068e9c3ddf7d0
github username <github email address>
2018-07-19 15:18:01 -0400

    everything we jsut did - almost done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -5,23 +5,55 @@ class FiguresController < ApplicationController
   end
 
   get '/figures' do
+    @figures = Figure.all
 
-    erb :'figures/index'
+    erb :'/figures/index'
   end
 
   get '/figures/new' do
 
-    erb :'figures/new'
+    erb :'/figures/new'
   end
 
-  get 'figures/:id/edit' do
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+# binding.pry
+    if params[:figure][:title_ids] != nil
+    params[:figure][:title_ids].each do |title|
+      FigureTitle.find_or_create_by(figure_id: @figure.id, title_id: title)
+    end
+  end
 
-    erb :'figures/edit'
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
+    end
+    @figure.save
+
+    if params[:figure][:landmark_ids] != nil
+    params[:figure][:landmark_ids].each do |lm_id|
+      Landmark.find(lm_id).update(figure_id: @figure.id)
+    end
+  end
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"], figure_id: @figure.id, year_completed: params["landmark"]["year_completed"])
+    end
+
+    @figure.save
+
+    redirect to :"/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+
+    erb :'/figures/edit'
   end
 
   get '/figures/:id' do
 
-    erb :'figures/show'
+    @figure = Figure.find(params[:id])
+
+    erb :'/figures/show'
   end
 
 
