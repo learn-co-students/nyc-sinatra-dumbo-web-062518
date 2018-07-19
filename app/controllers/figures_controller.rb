cd4ee9ca44780f181e85cf0841a244316b540900
github username <github email address>
2018-07-19 13:50:36 -0400

    controller form done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,29 @@
 class FiguresController < ApplicationController
-  
+
+  get '/' do
+    landing
+  end
+
+  get '/figures' do
+
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+
+    erb :'figures/new'
+  end
+
+  get 'figures/:id/edit' do
+
+    erb :'figures/edit'
+  end
+
+  get '/figures/:id' do
+
+    erb :'figures/show'
+  end
+
+
+
 end
