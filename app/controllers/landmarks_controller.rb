efe7f533237acea679e418260928b33d00a5a9bf
github username <github email address>
2018-07-19 12:00:48 -0400

    form is working

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,27 @@ class LandmarksController < ApplicationController
     landing
   end
 
+  get '/landmarks' do
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    binding.pry
+    @landmark = Landmark.create(params)
+
+    redirect to :"/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.all.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
+
 end
