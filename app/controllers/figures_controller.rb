c8be787acdc77545185aa00cd78f1be5f06ce012
alexneustein <alex.neustein@gmail.com>
2018-07-19 15:40:19 -0400

    updated edit post

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -59,7 +59,33 @@ class FiguresController < ApplicationController
   end
 
   patch "/figures" do
-    
+
+    @figure = Figure.create(params[:figure])
+# binding.pry
+    if params[:figure][:title_ids] != nil
+    params[:figure][:title_ids].each do |title|
+      FigureTitle.find_or_create_by(figure_id: @figure.id, title_id: title)
+    end
+  end
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.find_or_create_by(name: params["title"]["name"])
+    end
+    @figure.save
+
+    if params[:figure][:landmark_ids] != nil
+    params[:figure][:landmark_ids].each do |lm_id|
+      Landmark.find(lm_id).update(figure_id: @figure.id)
+    end
+  end
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.find_or_create_by(name: params["landmark"]["name"], figure_id: @figure.id, year_completed: params["landmark"]["year_completed"])
+    end
+
+    @figure.save
+
+    redirect to :"/figures/#{@figure.id}"
 
   end
 
